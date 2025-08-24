6ef230784e859fe9bf782d8954ed1011cc3abde6
basyair7 <basyair.fathul@gmail.com>
2025-08-25 00:52:50 +0700

    remove commands file

--- deploy-preview.ps1
+++
@@ -1,7 +0,0 @@
-Get-Content .env | ForEach-Object {
-  if ($_ -match "^(?<key>[^=]+)=(?<val>.+)$") {
-    [System.Environment]::SetEnvironmentVariable($matches['key'], $matches['val'])
-  }
-}
-
-deployctl deploy --project=kotoba-web --entrypoint=main.ts
